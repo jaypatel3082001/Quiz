463f8972bc4ae4ea44fccc2f397f6b0af38afa93
jaypatel3082001 <86408333+jaypatel3082001@users.noreply.github.com>
2024-06-07 18:29:25 +0530

    all curde operation done

--- index.js
+++ index.js
@@ -29,56 +29,7 @@ app.use(bodyParser.json());
 app.use('/auth',authRoute)
 app.use('/questions',question)
 app.use('/quize',quize)
-// app.use('/section',section)
-// app.use('/groups',group)
-// app.get('/',)
-// app.post('/post',(req,res)=>{
-//     const { username, password } = req.body;
-
-//     // Check if the username and password are provided
-//     if (!username || !password) {
-//         return res.status(400).json({ message: 'Username and password are required.' });
-//     }
-//    const users=[{
-//         "username":"admin1",
-//         "password":"123456"
-//     }]
-
-//     // Find user in the simulated database
-//     const user = users.find(u => u.username === username && u.password === password);
-
-//     // If user not found, return error
-//     if (!user) {
-//         return res.status(401).json({ message: 'Invalid username or password.' });
-//     }
-
-//     // If user found, return success message
-//     res.status(200).json({ message: 'Login successful!' });
-// })
-// Parse JSON bodies
-// app.use('')
-// app.post('/post',(req,res)=>{
-//     // console.log(req.body.username)
-//     const { username, password } = req.body;
-    
-
-//     // Check if the username and password are provided
-//     if (!username || !password) {
-//         return res.status(400).json({ message: 'Username and password are required.' });
-//     }
-
-
-//     // Find user in the simulated database
-//     const user = users.find(u => u.username === username && u.password === password.toString()); // Corrected: Convert password to string for comparison
-
-//     // If user not found, return error
-//     if (!user) {
-//         return res.status(401).json({ message: 'Invalid username or password.' });
-//     }
-
-//     // If user found, return success message
-//     res.status(200).json({ message: 'Login successful!' });
-// })
+app.use('/section',section)
 
 app.listen(port,()=>{
     console.log("port is working")
