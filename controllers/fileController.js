60bddd1d534366a4f397d60ac30fb56ae41d74a3
jaypatel3082001 <86408333+jaypatel3082001@users.noreply.github.com>
2024-07-11 15:00:14 +0530

    resultaPPijjjhhjjjjjjjjjgggggggggyykjqhggffgggggggggg

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,38 +1,64 @@
 const xlsx = require("xlsx");
-const path = require("path");
-const Questions = require("../models/questions");
+const fetch = require("node-fetch"); // If not already installed, install it
+const { b2 } = require("../middleware/multerMiddle");
+const fs = require("fs");
+
+const bucketId = "947d64b3985929e583fc0f12";
+const bucketName = "KT-developer";
 
 async function UploadquestionFile(req, res) {
   try {
+    const file = req.file; // Extract file information from request
+    const fileName = req.file.originalname;
     const filePath = req.file.path;
-    const workbook = xlsx.readFile(filePath);
-    const sheetName = workbook.SheetNames[0];
-    const sheet = workbook.Sheets[sheetName];
-    const data = xlsx.utils.sheet_to_json(sheet);
+    // const fileName = `${file.fieldname}-${Date.now()}${path.extname(
+    //   file.originalname
+    // )}`;
+    // Authorize with Backblaze B2
+    await b2.authorize();
 
-    const { question, option1, option2, option3, option4, answer } = req.body;
-
-    // Check if username exists using the User model
-    // const existingQuestion = await Questions.findOne({ question });
-    // if (existingQuestion) {
-    //     return res.status(400).json({ message: 'question already exists' });
-    // }
-    const nwewArray = data.filter(async (ele) => {
-      await Questions.create({
-        question: ele.question,
-        option1: ele.option1,
-        option2: ele.option2,
-        option3: ele.option3,
-        option4: ele.option4,
-        answer: ele.answer,
-      });
+    // Get upload URL
+    const {
+      data: { uploadUrl, authorizationToken },
+    } = await b2.getUploadUrl({
+      bucketId: bucketId,
     });
-    //   const questions =
 
-    res.json({ data: nwewArray });
+    const myFile = fs.readFileSync(filePath);
+    await b2.uploadFile({
+      uploadUrl: uploadUrl,
+      uploadAuthToken: authorizationToken,
+      fileName: "upload" + "/" + fileName,
+      data: myFile,
+    });
+
+    // // Upload file to Backblaze B2
+    // const uploadResponse = await b2.uploadFile({
+    //   uploadUrl,
+    //   uploadAuthToken: authorizationToken,
+    //   fileName,
+    //   data: file.buffer,
+    // });
+
+    // Construct the file download URL
+    // const fileDownloadUrl = `https://f000.backblazeb2.com/file/${bucketName}/upload/${fileName}`;
+
+    // // Fetch the file from the download URL
+    // const response = await fetch(fileDownloadUrl);
+    // const arrayBuffer = await response.arrayBuffer();
+    // const buffer = Buffer.from(arrayBuffer);
+
+    // // Process the file using xlsx
+    // const workbook = xlsx.read(buffer);
+    // const sheetName = workbook.SheetNames[0];
+    // const sheet = workbook.Sheets[sheetName];
+    // const data = xlsx.utils.sheet_to_json(sheet);
+
+    res.status(201).json("sucess");
   } catch (err) {
-    console.log(err);
-    res.status(500).send("Error uploading file");
+    console.error(err);
+    res.status(500).send(`Error uploading file ${err}`);
   }
 }
+
 module.exports = { UploadquestionFile };
--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -1,33 +1,9 @@
 const express = require("express");
 const { upload } = require("../middleware/multerMiddle");
 const { UploadquestionFile } = require("../controllers/fileController");
-// const multer = require("multer");
-// const xlsx = require("xlsx");
-// const path = require("path");
-// const fs = require("fs");
-// const app = express();
+
 const router = express.Router();
 
-// app.use("/images", express.static(path.join(__dirname, "public/images")));
-// // if (!fs.existsSync(uploadsDir)) {
-// //   fs.mkdirSync(uploadsDir);
-// // }
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/images");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(
-//       null,
-//       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-//     );
-//   },
-// });
-
-// //   const upload = multer({ storage: storage });
-
-// const upload = multer({ storage: storage });
-// router.post("/uplodfile", upload.single("file"), UploadFile);
-
 router.post("/questionupload", upload.single("file"), UploadquestionFile);
+
 module.exports = router;
