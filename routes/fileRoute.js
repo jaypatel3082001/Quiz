df2ee953d5d28b2431a1a3bdeb0a263e1bb68647
jaypatel3082001 <86408333+jaypatel3082001@users.noreply.github.com>
2024-07-19 15:06:53 +0530

    sdfhdfh

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -3,14 +3,18 @@ const { upload } = require("../middleware/multerMiddle");
 const {
   UploadquestionFile,
   getFileBackblazeByName,
-  Uploadss,
 } = require("../controllers/fileController");
 const { middlewareAuth } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 // router.use(middlewareAuth);
-router.post("/questionupload", upload.single("file"),middlewareAuth, UploadquestionFile);
-router.post("/ssupload", upload.single("file"), Uploadss);
-router.get("/questioread/:fileName", middlewareAuth,getFileBackblazeByName);
+router.post(
+  "/questionupload",
+  upload.single("file"),
+  middlewareAuth,
+  UploadquestionFile
+);
+// router.post("/ssupload", upload.single("file"), Uploadss);
+router.get("/questioread/:fileName", middlewareAuth, getFileBackblazeByName);
 
 module.exports = router;
